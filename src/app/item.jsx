c3f14f8db3f16c94730926f2485b62d639c62fd2
Miguel Ángel Figueroa García <miguel.angel.fg@gmail.com>
2025-11-29 13:19:36 +0000

    added client CRUD

--- src/app/item.jsx
+++ src/app/item.jsx
@@ -1,12 +1,36 @@
 // item.js
 
-export default function Item({title, price}){
-    return (
-        <div className="category">
-            <span className="item-price">{price}</span>
-            <span className="item-title">{title}</span>
-            
-            
-        </div>
-    );
+export default function Item({ title, price, editMode, setItem, removeItem }) {
+  const item = { title, price };
+  return (
+    <div className="item">
+      <span className="item-title">
+        {editMode ? (
+          <input
+            placeholder="Item title"
+            type="text"
+            value={title}
+            onChange={(ev) => setItem({ ...item, title: ev.target.value })}
+          />
+        ) : (
+          title
+        )}
+      </span>
+      <span className="item-price">
+        {editMode ? (
+          <>
+            <input
+              placeholder="Item price"
+              type="number"
+              value={price}
+              onChange={(ev) => setItem({ ...item, price: ev.target.value })}
+            />
+            <button onClick={removeItem}>Borrar</button>
+          </>
+        ) : (
+          price
+        )}
+      </span>
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 // page.js
+
+"use client";
+import { useState } from "react";
 import Category from "./category";
 
-const menu = {
-  title:"Camper Cafe",
+const menuConst = {
+  title: "Camper Cafe",
   subtitle: "Est. 2020",
   categories: [
     {
@@ -24,19 +27,74 @@ const menu = {
     },
   ],
 };
+const newCategory = {
+  title: "",
+  img: "",
+  items: [],
+};
+//const menuLS = JSON.parse(localStorage.getItem('menu'));
 
 export default function Home() {
+  const [menu, setMenu] = useState(/*menuLS ||*/ menuConst);
+  const [editMode, setEditMode] = useState(false);
+  const resetMenu = () => {
+    localStorage.setItem("menu", JSON.stringify(menuConst));
+    setMenu(menuConst);
+  };
+
+  const setCategory = (id) => (category) => {
+    const newCategories = [...menu.categories];
+    newCategories[id] = category;
+
+    setMenu({
+      ...menu,
+      categories: newCategories,
+    });
+  };
+
+  const removeCategory = (id) => () => {
+    if (
+      !window.confirm(
+        "¿Está seguro de eliminar la categoría y todos sus items?"
+      )
+    ) {
+      return;
+    }
+    const newCategories = [...menu.categories];
+    newCategories.splice(id, 1);
+    setMenu({
+      ...menu,
+      categories: newCategories,
+    });
+  };
+
   return (
     <div className="root">
+      <button onClick={() => setEditMode(!editMode)}>
+        {editMode ? "view mode" : "edit mode"}
+      </button>
+      <button onClick={resetMenu}>reset menu</button>
+
       <div className="menu">
         <div className="menu-header">
           <h1>{menu.title}</h1>
           <p className="subtitle">{menu.subtitle}</p>
         </div>
-        {menu.categories.map((c, idx) => <Category key={idx} {...c} />)}
-        <div className="separator"/>
+        {(editMode ? [...menu.categories, newCategory] : menu.categories).map(
+          (c, id) => (
+            <Category
+              key={id}
+              {...c}
+              editMode={editMode}
+              setCategory={setCategory(id)}
+              removeCategory={removeCategory(id)}
+            />
+          )
+        )}
+        <div className="separator" />
         <div className="footer">
-          <a href="#">Visit our website</a><br/>
+          <a href="#">Visit our website</a>
+          <br />
           123 Free Code Camp Drive
         </div>
       </div>
